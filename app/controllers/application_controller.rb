9016e2ff3ce7a5a1de5630ab4386ab7e2535e747
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-04-17 17:54:07 +0900

    application_controllerの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :firstname, :lastname, :firstname_kana, :lastname_kana, :date])
+  end
 end
+
+
+## application_controller
+## ビューファイル。
+## user.rb
