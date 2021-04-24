b417fd05508fa6878e7bc21458183cefadff36eb
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-04-24 17:38:46 +0900

    pending以下のコメントアウト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'nameが空では登録できない' do
+      @user.name = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      @user.email = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it 'passwordが空では登録できない' do
+      @user.password = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'passwordが6文字以下では登録できない' do
+      @user.password = '000000'
+      @user.password_confirmation = '000000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is invalid")
+    end
+    it 'firstnameが空では登録できない' do
+      @user.firstname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Firstname can't be blank")
+    end
+    it 'lastnameが空では登録できない' do
+      @user.lastname = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include( "Lastname can't be blank")
+    end
+    it 'firstname_kanaが空では登録できない' do
+      @user.firstname_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Firstname kana can't be blank")
+    end
+    it 'lastname_kanaが空では登録できない' do
+      @user.lastname_kana = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Lastname kana can't be blank")
+    end
+    it 'dateが空では登録できない' do
+      @user.date = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Date can't be blank")
+    end
+  end
 end
