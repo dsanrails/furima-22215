ac863df12ea5a6620a34f70c72879a751a73a070
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-04-24 21:00:59 +0900

    with_optionsの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-  validates :email, presence: true
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i }
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/ }
-  validates :date, presence: true
+  with_options presence: true do
+    validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :email
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i }
+    validates :firstname
+    validates :lastname
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :date
+  end
 end
