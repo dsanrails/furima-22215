01829a12aa2e8c484e012fc9e3b6506255ecbd79
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-04-25 20:47:51 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
     validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :email
     validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i }
-    validates :firstname
-    validates :lastname
-    validates :firstname_kana, format: { with: /\A[ァ-ヶー]+\z/ }
-    validates :lastname_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :firstname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :lastname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :date
   end
 end
