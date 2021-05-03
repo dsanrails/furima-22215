040be6d05faad8177889e06e06cf7aa36570eb82
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-03 20:43:52 +0900

    collection_selectの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :user
+  belongs_to :category
   has_one :purchase
   has_one_attached :image
 
