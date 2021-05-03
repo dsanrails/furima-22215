f434218e6e452cd13d53d8876f3f0b7070f23d80
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-03 16:43:24 +0900

    商品出品ページの表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
+
+  validates :items_name, :explanation, presence: true
+  validates :category_id, :status_id, :delicery_fee_id, :prefecture_id, :day_id, numericality: { other_than: 1 } 
 end
