4711b3c9890fc5b8ca700c5387ae329f5962797d
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-08 17:28:05 +0900

    アソシエーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :day
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_fee
+  belongs_to_active_hash :status
+
   belongs_to :category
   has_one :purchase
   has_one_attached :image
