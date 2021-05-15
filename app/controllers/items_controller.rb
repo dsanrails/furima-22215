46aa637692a6cf4c3dc515940570a413d431ad56
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-15 17:12:07 +0900

    redirect_itemメソッドの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_item, only: [:edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -22,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-      redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -42,5 +43,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+  
+  def redirect_item
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
