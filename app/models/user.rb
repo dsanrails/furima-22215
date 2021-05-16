a777c0636392bae56a399d3b318f52de50154e7e
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-16 14:34:02 +0900

    userモデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :items
+  
   with_options presence: true do
     with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
       validates :name, :firstname, :lastname
@@ -15,4 +15,5 @@ class User < ApplicationRecord
     validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]{6,128}+\z/i }
     validates :date
   end
+  
 end
