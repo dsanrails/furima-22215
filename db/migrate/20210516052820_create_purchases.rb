28326108f2404c801015f775a14a2873e30a1b8f
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-16 15:19:13 +0900

    テーブルの作成

--- db/migrate/20210516052820_create_purchases.rb
+++ db/migrate/20210516052820_create_purchases.rb
@@ -1,7 +1,8 @@
 class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
-
+      t.integer :user_id, forein_key: true
+      t.integer :item_id, forein_key: true
       t.timestamps
     end
   end
--- db/migrate/20210516052855_create_addresses.rb
+++ db/migrate/20210516052855_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code, null:false
+      t.integer :prefecture_id, null:false
+      t.string :municipality, null:false
+      t.string :address, null:false
+      t.string :building
+      t.string :tel, null:false
+      t.integer :purchase_id, forein_key: true
       t.timestamps
     end
   end
