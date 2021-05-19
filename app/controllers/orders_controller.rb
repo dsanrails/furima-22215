b9f5bf820c864c246bcbfaf9d2157b7e7db39355
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-19 21:16:27 +0900

    オーダーコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,28 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
+    @order = OrderForm.new
+  end
+
+  def create
+    @order_form = OrderForm.new(purchase_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to action: :index
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def purchase_params
+    params.permit(:postal_code, :prefecture_id, :municipality, :address, :building, :tel, :user_id, :item_id)
   end
 end
