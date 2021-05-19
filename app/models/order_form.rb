08c435d095801394c09a49c22ea30a6659435f3e
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-19 21:17:28 +0900

    saveメソッドの記述

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,7 +1,13 @@
-class Order < ApplicationRecord
-  include ActiveModel::Modele
+class OrderForm 
+  include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :tel, :purchase_id
   
+  validates :postal_code, :prefecture_id, :municipality, :address, :tel, presence: true
+  validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+  validates :tel, format: { with: /\A\d{11}\z/ }
+
   def save
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building: building, tel: tel, purchase_id: purchase_id)
+    Purchase.create(user_id: user_id, item_id: item_id)
   end
 end
