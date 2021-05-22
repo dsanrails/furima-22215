d599bbc314b460a318b5716683bc6261bd641c61
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-22 14:39:38 +0900

    キーの変更

--- spec/factories/purchases.rb
+++ spec/factories/purchases.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
-  factory :purchase do
-    
+  factory :order_form do
+    postal_code {'123-4567'}
+    prefecture_id {1}
+    municipality {'中央区'}
+    address {'丸の内'}
+    tel {'09012345678'}
   end
 end
