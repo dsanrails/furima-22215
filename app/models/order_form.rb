57c1ef2d8bf8ed9e2304ee903ae4e0080b60326d
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-27 20:47:16 +0900

    フォームオブジェクトの編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,15 +1,15 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :tel, :purchase_id, :token
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :tel, :token
 
   validates :postal_code, :prefecture_id, :municipality, :address, :tel, :user_id, :item_id, :token, presence: true
-  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ } # , message: "is invalid. Include hyphen(-)"}
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :tel, format: { with: /\A\d{10,11}\z/ }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
-                   building: building, tel: tel, purchase_id: purchase_id)
+                   building: building, tel: tel, purchase_id: purchase.id)
   end
 end
