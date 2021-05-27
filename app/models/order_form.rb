67314a683ab317f355dc2fb2f7164b7aea5a229b
dsanrails <77232860+dsanrails@users.noreply.github.com>
2021-05-27 18:53:59 +0900

    不要なバリデーションの削除

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,11 +2,11 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building, :tel, :purchase_id, :token
 
-  validates :postal_code, :prefecture_id, :municipality, :address, :tel, :user_id, :item_id, presence: true
+  validates :postal_code, :prefecture_id, :municipality, :address, :tel, :user_id, :item_id, :token, presence: true
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ } # , message: "is invalid. Include hyphen(-)"}
   validates :tel, format: { with: /\A\d{10,11}\z/ }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
-  validates :token, presence: true
+  
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
